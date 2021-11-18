de39235393f1d86656a6cde25261685290eae406
Escape <65812015+escape1004@users.noreply.github.com>
2021-11-18 17:19:20 +0900

    2021.11.18v1
    :D

---
+++ assets/script/mainMove.js
@@ -0,0 +1,598 @@
+function sec1TitleStart() {
+    // first
+    // left
+    gsap.to(".F1-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-19vw',
+        delay: 0
+    });
+    gsap.to(".R1-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-14.5vw',
+        delay: 0
+    });
+    gsap.to(".O1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-10.5vw',
+        delay: 0
+    });
+    gsap.to(".T1-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '2.44vw',
+        delay: 0
+    });
+    gsap.to(".E1-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '6vw',
+        delay: 0
+    });
+    gsap.to(".D1-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '15vw',
+        delay: 0
+    });
+
+    // right
+    gsap.to(".D1-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-15vw',
+        delay: 0
+    });
+    gsap.to(".N1-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-14vw',
+        delay: 0
+    });
+    gsap.to(".E1-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-6vw',
+        delay: 0
+    });
+    gsap.to(".T1-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-2.5vw',
+        delay: 0
+    });
+    gsap.to(".N1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '0.8vw',
+        delay: 0
+    });
+    gsap.to(".R1-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '11vw',
+        delay: 0
+    });
+    gsap.to(".F1-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '15.5vw',
+        delay: 0
+    });
+
+    // second
+    // left
+    gsap.to(".D2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-20vw',
+        delay: 0
+    });
+    gsap.to(".E2-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-14.8vw',
+        delay: 0
+    });
+    gsap.to(".E2-4", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-5.5vw',
+        delay: 0
+    });
+    gsap.to(".L2-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-1vw',
+        delay: 0
+    });
+    gsap.to(".P2-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '8vw',
+        delay: 0
+    });
+    gsap.to(".E2-5", {
+        duration: 1,
+        ease: "expo.out",
+        left: '12vw',
+        delay: 0
+    });
+    gsap.to(".R2-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '16.5vw',
+        delay: 0
+    });
+
+    // right
+    gsap.to(".R2-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-16.5vw',
+        delay: 0
+    });
+    gsap.to(".E2-6", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-15.3vw',
+        delay: 0
+    });
+    gsap.to(".P2-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-11.4vw',
+        delay: 0
+    });
+    gsap.to(".O2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-7vw',
+        delay: 0
+    });
+    gsap.to(".L2-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '1vw',
+        delay: 0
+    });
+    gsap.to(".E2-3", {
+        duration: 1,
+        ease: "expo.out",
+        right: '5.5vw',
+        delay: 0
+    });
+    gsap.to(".V2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '6.5vw',
+        delay: 0
+    });
+    gsap.to(".E2-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '14.8vw',
+        delay: 0
+    });
+
+    // third
+    // left
+    gsap.to(".P3-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-18.5vw',
+        delay: 0
+    });
+    gsap.to(".A3-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-15.38vw',
+        delay: 0
+    });
+    gsap.to(".R3-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-10vw',
+        delay: 0
+    });
+    gsap.to(".K3-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-5.2vw',
+        delay: 0
+    });
+    gsap.to(".S3", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-0.5vw',
+        delay: 0
+    });
+    gsap.to(".E3-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '4.5vw',
+        delay: 0
+    });
+    gsap.to(".I3", {
+        duration: 1,
+        ease: "expo.out",
+        left: '12.5vw',
+        delay: 0
+    });
+    gsap.to(".N3-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '18vw',
+        delay: 0
+    });
+
+    // right
+    gsap.to(".N3-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-18vw',
+        delay: 0
+    });
+    gsap.to(".J3", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-11vw',
+        delay: 0
+    });
+    gsap.to(".E3-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-4.5vw',
+        delay: 0
+    });
+    gsap.to(".K3-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '1.5vw',
+        delay: 0
+    });
+    gsap.to(".R3-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '6.5vw',
+        delay: 0
+    });
+    gsap.to(".A3-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '11.5vw',
+        delay: 0
+    });
+    gsap.to(".P3-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '18.5vw',
+        delay: 0
+    });
+
+    // sub
+    gsap.to(".bottom.left", {
+        duration: 2,
+        ease: "expo.out",
+        opacity: 1,
+        delay: 1
+    });
+    gsap.to(".bottom.right", {
+        duration: 2,
+        ease: "expo.out",
+        opacity: 1,
+        delay: 1.5
+    });
+
+    gsap.to(".bg-r", {
+        duration: 2,
+        ease: "expo.out",
+        opacity: 0,
+        delay: 2.5
+    });
+
+    // scrollTo
+    gsap.to(window, {
+        duration: 1,
+        scrollTo: "#section2",
+        delay: 4
+    });
+}
+
+function sec1TitleReturn() {
+    // first
+    // left
+    gsap.to(".F1-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-43.3vw',
+        delay: 0
+    });
+    gsap.to(".R1-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-39vw',
+        delay: 0
+    });
+    gsap.to(".O1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-35vw',
+        delay: 0
+    });
+    gsap.to(".T1-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-22vw',
+        delay: 0
+    });
+    gsap.to(".E1-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-18.5vw',
+        delay: 0
+    });
+    gsap.to(".D1-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-9.5vw',
+        delay: 0
+    });
+
+    // right
+    gsap.to(".D1-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-39.2vw',
+        delay: 0
+    });
+    gsap.to(".N1-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-38.2vw',
+        delay: 0
+    });
+    gsap.to(".E1-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-30.2vw',
+        delay: 0
+    });
+    gsap.to(".T1-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-26.5vw',
+        delay: 0
+    });
+    gsap.to(".N1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-23.3vw',
+        delay: 0
+    });
+    gsap.to(".R1-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-13vw',
+        delay: 0
+    });
+    gsap.to(".F1-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-8.5vw',
+        delay: 0
+    });
+
+    // second
+    // left
+    gsap.to(".D2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-44.3vw',
+        delay: 0
+    });
+    gsap.to(".E2-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-39.3vw',
+        delay: 0
+    });
+    gsap.to(".E2-4", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-30vw',
+        delay: 0
+    });
+    gsap.to(".L2-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-25.5vw',
+        delay: 0
+    });
+    gsap.to(".P2-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-16.5vw',
+        delay: 0
+    });
+    gsap.to(".E2-5", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-12.5vw',
+        delay: 0
+    });
+    gsap.to(".R2-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-8vw',
+        delay: 0
+    });
+
+    // right
+    gsap.to(".R2-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-40.5vw',
+        delay: 0
+    });
+    gsap.to(".E2-6", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-39vw',
+        delay: 0
+    });
+    gsap.to(".P2-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-35.3vw',
+        delay: 0
+    });
+    gsap.to(".O2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-31vw',
+        delay: 0
+    });
+    gsap.to(".L2-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-23vw',
+        delay: 0
+    });
+    gsap.to(".E2-3", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-18.5vw',
+        delay: 0
+    });
+    gsap.to(".V2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-17.5vw',
+        delay: 0
+    });
+    gsap.to(".E2-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-9.2vw',
+        delay: 0
+    });
+
+    // third
+    // left
+    gsap.to(".P3-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-43vw',
+        delay: 0
+    });
+    gsap.to(".A3-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-40vw',
+        delay: 0
+    });
+    gsap.to(".R3-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-34.5vw',
+        delay: 0
+    });
+    gsap.to(".K3-1", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-29.7vw',
+        delay: 0
+    });
+    gsap.to(".S3", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-25vw',
+        delay: 0
+    });
+    gsap.to(".E3-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-20vw',
+        delay: 0
+    });
+    gsap.to(".I3", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-12vw',
+        delay: 0
+    });
+    gsap.to(".N3-2", {
+        duration: 1,
+        ease: "expo.out",
+        left: '-6.5vw',
+        delay: 0
+    });
+
+    // right
+    gsap.to(".N3-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-42vw',
+        delay: 0
+    });
+    gsap.to(".J3", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-35vw',
+        delay: 0
+    });
+    gsap.to(".E3-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-28.5vw',
+        delay: 0
+    });
+    gsap.to(".K3-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-22.5vw',
+        delay: 0
+    });
+    gsap.to(".R3-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-17.5vw',
+        delay: 0
+    });
+    gsap.to(".A3-2", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-12.5vw',
+        delay: 0
+    });
+    gsap.to(".P3-1", {
+        duration: 1,
+        ease: "expo.out",
+        right: '-5.5vw',
+        delay: 0
+    });
+
+    // sub
+    gsap.to(".bottom.left", {
+        duration: 1.5,
+        ease: "expo.out",
+        opacity: 0,
+        delay: 0
+    });
+    gsap.to(".bottom.right", {
+        duration: 1.5,
+        ease: "expo.out",
+        opacity: 0,
+        delay: 0
+    });
+
+    gsap.to(".bg-r", {
+        duration: 2,
+        ease: "expo.out",
+        opacity: 1,
+        delay: 0
+    });
+}
